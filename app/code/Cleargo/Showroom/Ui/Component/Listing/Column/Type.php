0d6775762c232993c7226f878ace63b4ea97a167
Thomas Yau <thomas@cleargo.com>
2016-06-06 12:16:19 +0800

    showroom type renderer

---
+++ app/code/Cleargo/Showroom/Ui/Component/Listing/Column/Type.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Cleargo\Showroom\Ui\Component\Listing\Column;
+
+use Magento\Framework\Escaper;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Store\Model\System\Store as SystemStore;
+use Magento\Ui\Component\Listing\Columns\Column;
+
+/**
+ * Class Store
+ */
+class Type extends Column
+{
+    /**
+     * Escaper
+     *
+     * @var \Magento\Framework\Escaper
+     */
+    protected $escaper;
+
+    /**
+     * System type
+     *
+     * @var SystemStore
+     */
+    protected $systemStore;
+
+    /**
+     * Constructor
+     *
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param SystemStore $systemStore
+     * @param Escaper $escaper
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        SystemStore $systemStore,
+        Escaper $escaper,
+        array $components = [],
+        array $data = []
+    ) {
+        $this->systemStore = $systemStore;
+        $this->escaper = $escaper;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     * Prepare Data Source
+     *
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as & $item) {
+                $item[$this->getData('name')] = $this->prepareItem($item);
+            }
+        }
+
+        return $dataSource;
+    }
+
+    /**
+     * Get data
+     *
+     * @param array $item
+     * @return string
+     */
+    protected function prepareItem(array $item)
+    {
+        $content = '';
+
+/*var_dump($item);
+        die();*/
+        $origStores = $item['type_id'];
+
+        if (empty($origStores)) {
+            return '';
+        }
+        if (!is_array($origStores)) {
+            $origStores = [$origStores];
+        }
+
+        $countType =0;
+        foreach ($origStores as $type){
+            if($countType != 0){
+                $content .= '<br/>';
+            }
+            $countType ++ ;
+            switch ($type) {
+                case 1:
+                    $content .= 'Type A';
+                    break;
+                case 2:
+                    $content .= 'Type B';
+                    break;
+                case 3:
+                    $content .= 'Type C';
+                    break;
+
+                default:
+                    $content .= 'Type A';
+            }
+
+        }
+
+
+        return $content;
+    }
+}
