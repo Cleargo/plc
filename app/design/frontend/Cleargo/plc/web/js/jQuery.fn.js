db0a9bb5e1f4679cac1b232eff6adb0a95f3596b
gamayuen <gamaching@gmail.com>
2016-07-25 18:03:00 +0800

    Header style and layout

--- app/design/frontend/Cleargo/plc/web/js/jQuery.fn.js
+++ app/design/frontend/Cleargo/plc/web/js/jQuery.fn.js
@@ -1,5 +1,11 @@
 require(['jquery'], function($) {
     //console.log("test");jQuery(function(){
+    if (jQuery(window).width() < 768){
+        jQuery("#topnav .level0 a").click(function(e){
+            e.preventDefault();
+            window.location = jQuery(this).attr('href');
+        });
+    }
     
     jQuery(".top-links.block-user a.action").click(function(){
         jQuery(".top-links.block-user .block-content").toggle();
@@ -8,7 +14,8 @@ require(['jquery'], function($) {
 
     jQuery(".top-links.block-search a.action").click(function() {
         if (jQuery(window).width() < 768){
-            jQuery(".top-links.block-search .block-content").toggle("fast");
+            jQuery(".top-links.block-search .block-content").toggle();
+            jQuery(".top-links.block-search .block-content label.label").toggleClass("active");
         }else{
             jQuery(".top-links.block-search .block-content").toggle("fast");
         }
@@ -22,7 +29,12 @@ require(['jquery'], function($) {
             console.log('outside select');
         }
         if(jQuery(event.target).parents(".top-links.block-search").length==0 && jQuery(".top-links.block-search a.action").hasClass('active')){
-            jQuery(".top-links.block-search .block-content").hide("fast");
+            if (jQuery(window).width() < 768){
+                jQuery(".top-links.block-search .block-content").hide();
+                jQuery(".top-links.block-search .block-content label.label").removeClass("active");
+            }else{
+                jQuery(".top-links.block-search .block-content").hide('fast');
+            }
             jQuery(".top-links.block-search a.action").removeClass("active");
         }        
     })
