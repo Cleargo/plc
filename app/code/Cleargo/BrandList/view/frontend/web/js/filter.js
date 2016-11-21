5e798ac84fb8a765f426d7453d7419e9d93cb58c
Thomas Yau <thomas@cleargo.com>
2016-11-21 13:40:38 +0800

    BrandList add header active js

--- app/code/Cleargo/BrandList/view/frontend/web/js/filter.js
+++ app/code/Cleargo/BrandList/view/frontend/web/js/filter.js
@@ -5,6 +5,8 @@ require([
     $(document).ready(function() {
         $('#brandList10').show();
         $('.cat-top-nav').find('li').on('click',function () {
+            $('.cat-top-nav').find('a').removeClass('active');
+            $(this).find('a').addClass('active');
             $('.brands_list').hide();
             $('#brandList' + $(this).attr('target')).show();
         });
