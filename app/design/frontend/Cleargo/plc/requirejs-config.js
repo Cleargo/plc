1e47e729b66168e2a5d76abf8eb6aaf3c1412182
Thomas Yau <thomas@cleargo.com>
2016-11-21 18:14:40 +0800

    add bootstrap multiselect

--- app/design/frontend/Cleargo/plc/requirejs-config.js
+++ app/design/frontend/Cleargo/plc/requirejs-config.js
@@ -3,9 +3,14 @@ var config = {
         "jquery.fancybox":"js/fancybox/jquery.fancybox",
         "jquery.mCustomScrollbar":"js/mCustomScrollbar/jquery.mCustomScrollbar.concat.min",
         "jquery.touchwipe":"js/jquery.touchwipe.1.1.1",
+        "bootstrap-multiselect":"js/bootstrap-multiselect",
+        "bootstrap":"js/bootstrap.min",
         "plc.fn":"js/plc.fn"
     },
     "shim": {
+        "bootstrap":{
+            "deps":['jquery']
+        },
         "jquery.fancybox":{
             "deps":['jquery']
         },
@@ -14,6 +19,9 @@ var config = {
         },
         "jquery.touchwipe":{
             "deps":['jquery']
+        },
+        "bootstrap-multiselect":{
+            "deps":['jquery',"bootstrap"]
         },
         "plc.fn":{
             "deps":['jquery']
