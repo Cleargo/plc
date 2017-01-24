0d00a4d18fbba4763adb8febff60847234ebfce0
Thomas Yau <thomas@cleargo.com>
2017-01-24 12:21:48 +0800

    fix pdf chinese bug

---
+++ app/code/Cleargo/AdminTheme/Model/Order/Pdf/Creditmemo.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Thomas
+ * Date: 24/1/2017
+ * Time: 12:09 PM
+ */
+namespace Cleargo\AdminTheme\Model\Order\Pdf;
+
+use Magento\Sales\Model\Order\Pdf\Creditmemo as OriginalCreditmemo;
+
+
+
+class Creditmemo extends OriginalCreditmemo{
+    /**
+     * Set font as regular
+     *
+     * @param  \Zend_Pdf_Page $object
+     * @param  int $size
+     * @return \Zend_Pdf_Resource_Font
+     */
+    protected function _setFontRegular($object, $size = 7)
+    {
+        $font = \Zend_Pdf_Font::fontWithPath(
+            $this->_rootDirectory->getAbsolutePath('lib/internal/LinLibertineFont/kaiu.ttf')
+        );
+        $object->setFont($font, $size);
+        return $font;
+    }
+
+    /**
+     * Set font as bold
+     *
+     * @param  \Zend_Pdf_Page $object
+     * @param  int $size
+     * @return \Zend_Pdf_Resource_Font
+     */
+    protected function _setFontBold($object, $size = 7)
+    {
+        $font = \Zend_Pdf_Font::fontWithPath(
+            $this->_rootDirectory->getAbsolutePath('lib/internal/LinLibertineFont/kaiu.ttf')
+        );
+        $object->setFont($font, $size);
+        return $font;
+    }
+
+    /**
+     * Set font as italic
+     *
+     * @param  \Zend_Pdf_Page $object
+     * @param  int $size
+     * @return \Zend_Pdf_Resource_Font
+     */
+    protected function _setFontItalic($object, $size = 7)
+    {
+        $font = \Zend_Pdf_Font::fontWithPath(
+            $this->_rootDirectory->getAbsolutePath('lib/internal/LinLibertineFont/kaiu.ttf')
+        );
+        $object->setFont($font, $size);
+        return $font;
+    }
+}
