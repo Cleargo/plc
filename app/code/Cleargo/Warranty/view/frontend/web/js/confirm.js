629d402a8ebbfd071dcdb78b4a300dda926c34a3
Thomas Yau <thomas@cleargo.com>
2017-08-14 11:35:19 +0800

    mt5 popup only appear for guest

--- app/code/Cleargo/Warranty/view/frontend/web/js/confirm.js
+++ app/code/Cleargo/Warranty/view/frontend/web/js/confirm.js
@@ -1,25 +1,33 @@
 require([
-    'jquery','Magento_Ui/js/modal/confirm'
-], function($,confirmation){
+    'jquery','Magento_Ui/js/modal/confirm', 'Magento_Customer/js/customer-data'
+], function($,confirmation,customerData){
     $(document).ready(function(){
+        //customerData.reload();
+        var customer = customerData.get('customer');
+        console.log( window.isCustomerLoggedIn );
         $("#warrantRegContainer").find(".registration-agree").click(function (event) {
-            var nextPage = window.location.origin + "/warranty/form/index";
-            var loginPage = window.location.origin + "/customer/account/login";
-            confirmation({
-                title: $.mage.__('Friendly Reminder') ,
-                content: $.mage.__('For users who have registered PLC eShop account, please login first before you fill in the warranty form.<br/>Or you can choose to proceed as a guest.') ,
-                actions: {
-                    confirm: function(){
-                        window.location.href = nextPage;
-                    },
-                    cancel: function(){
-                        window.location.href = loginPage;
+            if(!window.isCustomerLoggedIn ){
+                var nextPage = window.location.origin + "/warranty/form/index";
+                var loginPage = window.location.origin + "/customer/account/login";
+                confirmation({
+                    title: $.mage.__('Friendly Reminder') ,
+                    content: $.mage.__('For users who have registered PLC eShop account, please login first before you fill in the warranty form.<br/>Or you can choose to proceed as a guest.') ,
+                    actions: {
+                        confirm: function(){
+                            window.location.href = nextPage;
+                        },
+                        cancel: function(){
+                            window.location.href = loginPage;
+                        },
+                        closed: function () {
+                            return false;
+                        }
                     }
-                }
-            });
-            $(".action-accept").text($.mage.__('Proceed as Guest'));
-            $(".action-dismiss").text($.mage.__('Login'));
-            return false;
+                });
+                $(".action-accept").text($.mage.__('Proceed as Guest'));
+                $(".action-dismiss").text($.mage.__('Login'));
+                return false;
+            }
         });
     });
 });
