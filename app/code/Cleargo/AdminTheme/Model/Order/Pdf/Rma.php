9eab53c887b57a553ef3b16a6f59c5856bd34de4
SUNNYMAN <waichiu0304@gmail.com>
2017-09-11 15:41:16 +0800

    bug #122 fixed

--- app/code/Cleargo/AdminTheme/Model/Order/Pdf/Rma.php
+++ app/code/Cleargo/AdminTheme/Model/Order/Pdf/Rma.php
@@ -59,4 +59,45 @@ class Rma extends OriginalRma{
         $object->setFont($font, $size);
         return $font;
     }
+
+    protected function _drawRmaItem($item, $page)
+    {
+        $productName = $this->string->split($item->getProductName(), 60, true, true);
+        $productName = isset($productName[0]) ? $productName[0] : '';
+
+        $page->drawText($productName, $this->getProductNameX(), $this->y, 'UTF-8');
+
+        $productSku = $this->string->split($item->getProductSku(), 25);
+        $productSku = isset($productSku[0]) ? $productSku[0] : '';
+        $page->drawText($productSku, $this->getProductSkuX(), $this->y, 'UTF-8');
+
+        $condition = $this->string->split($this->_getOptionAttributeStringValue($item->getCondition()), 25);
+        $page->drawText($condition[0], $this->getConditionX(), $this->y, 'UTF-8');
+
+        $resolution = $this->string->split($this->_getOptionAttributeStringValue($item->getResolution()), 25);
+
+        if($resolution == null){
+            $resolution[0] = '';
+        }
+
+        $page->drawText($resolution[0], $this->getResolutionX(), $this->y, 'UTF-8');
+        $page->drawText(
+            $this->_rmaData->parseQuantity($item->getQtyRequested(), $item),
+            $this->getQtyRequestedX(),
+            $this->y,
+            'UTF-8'
+        );
+
+        $page->drawText($this->_rmaData->getQty($item), $this->getQtyX(), $this->y, 'UTF-8');
+
+        $status = $this->string->split($item->getStatusLabel(), 25);
+        $page->drawText($status[0], $this->getStatusX(), $this->y, 'UTF-8');
+
+        $productOptions = $item->getOptions();
+        if (is_array($productOptions) && !empty($productOptions)) {
+            $this->_drawCustomOptions($productOptions, $page);
+        }
+
+        $this->y -= 10;
+    }
 }
