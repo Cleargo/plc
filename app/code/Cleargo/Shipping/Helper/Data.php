f781bff30aa3913676e8d68a18a0911f4a9233d9
SUNNYMAN <waichiu0304@gmail.com>
2017-10-19 10:32:06 +0800

    bug #143 fixed.

---
+++ app/code/Cleargo/Shipping/Helper/Data.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+// @codingStandardsIgnoreFile
+
+/**
+ * Shipping data helper
+ */
+namespace Cleargo\Shipping\Helper;
+
+class Data extends \Magento\Shipping\Helper\Data
+{
+    protected function _getTrackingUrl($key, $model, $method = 'getId')
+    {
+        $urlPart = "{$key}:{$model->{$method}()}:{$model->getProtectCode()}";
+        $params = [
+            '_direct' => 'shipping/tracking/popup',
+            '_query' => ['hash' => $this->urlEncoder->encode($urlPart)]
+        ];
+
+        $cur_storeid =  $this->_storeManager->getStore()->getStoreId();
+        $storeModel = $this->_storeManager->getStore($cur_storeid);
+        return $storeModel->getUrl('', $params);
+    }
+}
